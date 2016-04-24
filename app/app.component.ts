5059f89b9af552799c49a385bb385ead65f2b90a
Matt Swensen <mjswensen@Matts-MacBook-Pro.local>
2016-04-23 21:49:04 -0600

    Better bindings for fill colors.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,13 @@
 import {Component} from 'angular2/core';
 import {BackgroundComponent} from './background.component';
+
+class FillColor {
+  constructor(value: string, name: string) {
+    this.value = value;
+    this.name = name;
+  }
+}
+
 @Component({
     selector: 'lucid-nametags',
     templateUrl: './app/app.component.html',
@@ -7,11 +15,22 @@ import {BackgroundComponent} from './background.component';
     directives: [BackgroundComponent],
 })
 export class AppComponent {
+
+  fillColors: FillColor[] = [
+    new FillColor('#29AAE1', 'Lucid blue'),
+    new FillColor('#FC8D2A', 'Lucidchart orange'),
+    new FillColor('#8CBF3F', 'Lucidpress green'),
+    new FillColor('#3D4752', 'Lucid blue steel'),
+    new FillColor('#77818C', 'Lucid blue steel 2'),
+    new FillColor('#2C84B5', 'Lucid blue dark'),
+    new FillColor('CUSTOM', 'Custom...'),
+  ];
+
   firstName: string;
   lastName: string;
   jobTitle: string;
   teamName: string;
-  fillColor: string;
+  fillColor: string = this.fillColors[0].value;
   customFillColor: string;
 
   print() {
